b51064f7687c8248828a41701cf3a5d05c7c4689
Vinmar Tipay <vinmar.tipay@outlook.com>
2026-01-07 11:46:18 +0000

    added unittests for dto_utils

--- src/utils/dto_utils.rs
+++ src/utils/dto_utils.rs
@@ -7,3 +7,24 @@ pub fn validate_name(name: &str) -> Result<(), ValidationError>{
     }
     Ok(())
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn name_without_decimals_is_valid(){
+        let result = validate_name("John");
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn name_with_decimals_is_invalid(){
+        let result = validate_name("J0hn");
+        assert!(result.is_err());
+
+        let err = result.unwrap_err();
+        assert_eq!(err.code, "Must not contain numbers");
+    }
+}
